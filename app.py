30a9072c4f1c8931f44776e2e4d09871ff0dd9e0
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2019-07-05 19:39:15 -0700

    added main html template and functionality

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, make_response, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+
+@app.route('/set')
+def setCookie():
+    response = make_response('set cookie')
+    response.set_cookie('framework', 'flask')
+    return response
+
+
+@app.route('/get')
+def getCookie():
+    framework = request.cookies.get('framework')
+    return 'framework: ' + framework
+
+
 
 
 if __name__ == '__main__':
