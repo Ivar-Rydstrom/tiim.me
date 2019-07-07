3595eb51247cabc089a18e68b6c075f2e36625f8
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2019-07-07 12:16:27 -0700

    added basic event display to document

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, make_response, request
+from flask import Flask, render_template, url_for, make_response, request, redirect
 import datetime
 import json
 from event_json import eventToJson, jsonToEvents, eventToDict, dictToEvent
@@ -20,25 +20,28 @@ def index():
             form.get('end_time', default_name))
 
         if 'events' in request.cookies:
-            response = make_response(render_template('index.html'))
             events_json = request.cookies.get('events')
             events = jsonToEvents(events_json)
             events.append(eventToDict(event))
+            response = make_response(redirect('/'))
             response.set_cookie('events', json.dumps(events))
             return response
 
         else:
-            response = make_response(render_template('index.html'))
             events = []
             events.append(eventToDict(event))
+            response = make_response(render_template('index.html', events=events))
             response.set_cookie('events', json.dumps(events))
             return response
 
     else:
-        events_json = request.cookies.get('events')
-        events = jsonToEvents(events_json)
-        dict = events
-        return render_template('index.html', events=events)
+        if 'events' in request.cookies:
+            events_json = request.cookies.get('events')
+            events = jsonToEvents(events_json)
+            return render_template('index.html', events=events)
+
+        else:
+            return render_template('index.html')
 
 
 if __name__ == '__main__':
