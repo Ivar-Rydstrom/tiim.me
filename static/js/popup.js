ca8848d5622fe7bb3773c53fef5e0b733184c153
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2019-08-27 19:48:56 -0700

    modified html structure to accomidate for popup menu

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,12 +1,15 @@
 
 var popup_settings = document.getElementById("popup-settings");
 var nav_settings = document.getElementById("nav-settings");
+var blur = document.getElementById("background-blur");
 
 nav_settings.onmouseenter = function() {
   popup_settings.style.display = "block";
+  blur.style.width = "100%";
 }
 nav_settings.onmouseout = function() {
   popup_settings.style.display = "none";
+  blur.style.width = "0%";
 }
 
 
@@ -15,9 +18,11 @@ var nav_help = document.getElementById("nav-help");
 
 nav_help.onmouseenter = function() {
   popup_help.style.display = "block";
+  blur.style.width = "100%";
 }
 nav_help.onmouseout = function() {
   popup_help.style.display = "none";
+  blur.style.width = "0%";
 }
 
 
@@ -26,7 +31,9 @@ var nav_information = document.getElementById("nav-information");
 
 nav_information.onmouseenter = function() {
   popup_information.style.display = "block";
+  blur.style.width = "100%";
 }
 nav_information.onmouseout = function() {
   popup_information.style.display = "none";
+  blur.style.width = "0%";
 }
